8b53aaa0db582cdcca64d23b2ee2a043ddba1665
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2018-10-10 15:42:10 +1000

    add file left out of older commits

---
+++ libs/colours.rb
@@ -0,0 +1,8 @@
+
+def R(stuff) "\x1B[31m#{stuff}\x1B[0m"; end
+def G(stuff) "\x1B[32m#{stuff}\x1B[0m"; end
+def B(stuff) "\x1B[34m#{stuff}\x1B[0m"; end
+def C(stuff) "\x1B[36m#{stuff}\x1B[0m"; end
+def M(stuff) "\x1B[35m#{stuff}\x1B[0m"; end
+def Y(stuff) "\x1B[93m#{stuff}\x1B[0m"; end
+
