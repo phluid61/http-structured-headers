a0b443e46c9832abb4d25187de0f8aec019e2b1f
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2018-11-22 10:17:33 +1000

    add more colour codes

--- libs/colours.rb
+++ libs/colours.rb
@@ -5,4 +5,16 @@ def B(stuff) "\x1B[34m#{stuff}\x1B[0m"; end
 def C(stuff) "\x1B[36m#{stuff}\x1B[0m"; end
 def M(stuff) "\x1B[35m#{stuff}\x1B[0m"; end
 def Y(stuff) "\x1B[93m#{stuff}\x1B[0m"; end
+def N(stuff) "\x1B[33m#{stuff}\x1B[0m"; end
+def W(stuff) "\x1B[97m#{stuff}\x1B[0m"; end
+def K(stuff) "\x1B[30m#{stuff}\x1B[0m"; end
 
+def R!(stuff) "\x1B[41m#{stuff}\x1B[0m"; end
+def G!(stuff) "\x1B[42m#{stuff}\x1B[0m"; end
+def B!(stuff) "\x1B[44m#{stuff}\x1B[0m"; end
+def C!(stuff) "\x1B[46m#{stuff}\x1B[0m"; end
+def M!(stuff) "\x1B[45m#{stuff}\x1B[0m"; end
+def Y!(stuff) "\x1B[103m#{stuff}\x1B[0m"; end
+def N!(stuff) "\x1B[43m#{stuff}\x1B[0m"; end
+def W!(stuff) "\x1B[107m#{stuff}\x1B[0m"; end
+def K!(stuff) "\x1B[40m#{stuff}\x1B[0m"; end
